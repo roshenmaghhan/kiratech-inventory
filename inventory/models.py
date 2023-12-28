936a1e85cd2f3b11af5798cd311b549da3e3964e
roshenmaghhan <roshenmaghhan@gmail.com>
2023-12-28 19:25:26 +0800

    feat:Added constraints to models

--- inventory/models.py
+++ inventory/models.py
@@ -1,19 +1,20 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Supplier model class
 class Supplier(models.Model) : 
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     
     def __str__(self) :
         return self.name
 
 # Inventory model class
 class Inventory(models.Model) : 
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
-    note = models.TextField()
-    stock = models.IntegerField()
-    availability = models.BooleanField()
+    name = models.CharField(max_length=255, null=False)
+    description = models.CharField(max_length=255, null=False)
+    note = models.TextField() # Note should be optional
+    stock = models.IntegerField(validators=[MinValueValidator(0)]) # Stock shouldn't be a negative number (?)
+    availability = models.BooleanField(default=True)
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
 
     def __str__(self) : 
